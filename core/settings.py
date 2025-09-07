5c4d3cc0dc9ed505611176bde1c3a91e1a08cc05
Alinur121118 <alinur121118@gmail.com>
2025-09-07 11:39:42 +0600

    for server 2

--- core/settings.py
+++ core/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = [BASE_DIR / 'static']
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
